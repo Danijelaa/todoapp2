ecc1cdd228077a5e662262e5b815983f0422cd7d
danijela <danijeladrljaca87@gmail.com>
2019-06-06 23:05:12 +0200

    Created UserLoginDao Bean.

---
+++ src/main/java/com/todoapp/dao/UserLoginDao.java
@@ -0,0 +1,30 @@
+package com.todoapp.dao;
+
+public class UserLoginDao {
+
+	private String username;
+	private String password;
+	
+	public UserLoginDao() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
